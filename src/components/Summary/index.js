153f7f125431d7d476bd05303e44461cc05fd54b
maximilianogom-lagash <57406898+maximilianogom-lagash@users.noreply.github.com>
2020-01-17 09:25:39 -0300

    Ultimo commit Maxi

--- src/components/Summary/index.js
+++ src/components/Summary/index.js
@@ -9,15 +9,23 @@ const Summary = ({ description, dateRange, hourType, project, hours }) => (
   <View style={{ flex: 1, flexDirection: 'column' }}>
     <Text style={styles.container}>Summary</Text>
     <View style={styles.container}>
-      <Title>Proyecto:</Title>
-      <Paragraph>{project}</Paragraph>
-      <Title>Horas:</Title>
-      <Paragraph>{hours}</Paragraph>
-      <Title>Tipo de hora:</Title>
-      <Paragraph>{hourType}</Paragraph>
-      <Title>Programar carga:</Title>
-      <Paragraph>Desde: {dateRange ? dateRange.startDate : ''}</Paragraph>
-      <Paragraph>Hasta: {dateRange ? dateRange.endDate : ''}</Paragraph>
+      <View style={styles.container2}>
+        <Title>Proyecto:</Title>
+        <Paragraph>{project}</Paragraph>
+      </View>
+      <View style={styles.container2}>
+        <Title>Horas:</Title>
+        <Paragraph>{hours}</Paragraph>
+      </View>
+      <View style={styles.container2}>
+        <Title>Tipo de hora:</Title>
+        <Paragraph>{hourType}</Paragraph>
+      </View>
+      <View style={styles.container2}>
+        <Title>Programar carga:</Title>
+        <Paragraph>Desde: {dateRange ? dateRange.startDate : ''}</Paragraph>
+        <Paragraph>Hasta: {dateRange ? dateRange.endDate : ''}</Paragraph>
+      </View>
     </View>
     <View style={{ flex: 1 }}>
       <LottieView
