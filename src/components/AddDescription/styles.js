c6a8bfe92adcdaae787d20d5f38f14da57de21bf
maximilianogom-lagash <57406898+maximilianogom-lagash@users.noreply.github.com>
2020-01-17 02:43:12 -0300

    Actualizar boton

--- src/components/AddDescription/styles.js
+++ src/components/AddDescription/styles.js
@@ -2,15 +2,13 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   Boton1: {
-    marginTop: 400,
-    paddingRight: 100,
     flexDirection: 'row',
     height: 50
   },
   Boton2: {
-    marginTop: 60,
-    paddingRight: 100,
     flexDirection: 'row',
     height: 50
   }
 });
+
+export default styles;
--- src/containers/Hours/index.js
+++ src/containers/Hours/index.js
@@ -22,16 +22,7 @@ const styles = StyleSheet.create({
 const Hours = () => (
   <AppIntro>
     <View style={[styles.slide, { backgroundColor: '#fa931d' }]}>
-      <View level={10}>
-        <Text style={styles.text}>Principal microfono</Text>
-        <AddDescription />
-      </View>
-      <View level={15}>
-        <Text style={styles.text}>Page 1</Text>
-      </View>
-      <View level={8}>
-        <Text style={styles.text}>Page 1</Text>
-      </View>
+      <AddDescription />
     </View>
     <View style={[styles.slide, { backgroundColor: '#a4b602' }]}>
       <View level={-10}>
