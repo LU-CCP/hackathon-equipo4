9da6318ad9d7f586729836a99671740fd6de27f8
Victoria Zanforlin <victoriaz@lagash.com>
2020-01-17 10:07:13 -0300

    .

--- src/config/debug.js
+++ src/config/debug.js
@@ -1,4 +1,4 @@
 if (__DEV__) {
   // eslint-disable-next-line no-console
-  console.disableYellowBox = false; // disable only for e2e test in debug mode
+  console.disableYellowBox = true; // disable only for e2e test in debug mode
 }
